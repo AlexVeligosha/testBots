be75ec87e8c4d251a87c793f457106fe4238a309
AlexVeligosha <rpyiiika@gmail.com>
2018-12-05 12:19:32 +0200

    update script

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "Some result">> ./result.txt
+output_file="log.txt"
+mkdir newman
+cd newman
+echo "environment = $1" > ./output_file
+echo "mode = $2" >> ./output_file
+echo "first = $3" >> ./output_file
+echo "quantity = $4" >> ./output_file
+echo "threads = $5" >> ./output_file
+echo "delay = $6" >> ./output_file
